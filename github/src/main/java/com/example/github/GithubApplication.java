495471b5078492d03564d6728f28e08531ff0525
zhouy <zhouy@xbnwl.onaliyun.com>
2023-04-27 09:12:44 +0800

    idea

--- github/src/main/java/com/example/github/GithubApplication.java
+++ github/src/main/java/com/example/github/GithubApplication.java
@@ -1,14 +1,12 @@
 package com.example.github;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+
 public class GithubApplication {
 
     public static void main(String[] args) {
         String a="a";
-        SpringApplication.run(GithubApplication.class, args);
     }
 
 }
